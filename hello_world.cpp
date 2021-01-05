bf7943115d7a861330ea8d382910e53347d38fd3
unknown <testgitlab@mail.ru>
2021-01-05 04:50:27 +0400

    Первоначальный коммит

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//using namespace std;
+int main()
+{   
+    char s[80];
+    cout << "Input user name: ";
+    cin >> s;
+    cout << "Hello world from@" << s << endl;
+	system("pause");
+    return 0;
+}
